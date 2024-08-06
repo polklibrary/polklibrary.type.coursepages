2ef5bddc08e7e3e2efcfbd4c3bc3e5bfeae65edc
David Hietpas <hietpasd@uwosh.edu>
2024-08-06 16:34:52 +0100

    added hook to new guides sys

--- trunk/polklibrary.type.coursepages/src/polklibrary/type/coursepages/browser/canvas_view.py
+++ trunk/polklibrary.type.coursepages/src/polklibrary/type/coursepages/browser/canvas_view.py
@@ -207,7 +207,7 @@ class CanvasView(BrowserView):
             'is_canvas_editor': self.is_canvas_editor,
         }
         
-        r = requests.get('https://polk.uwosh.edu/libhero/canvas/' + canvas_course_id, params=data)
+        r = requests.get('https://polk.uwosh.edu/libhero/canvas/' + self.canvas_course_id, params=data)
         return r.content
 
 
